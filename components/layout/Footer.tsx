03cf89cf73e8ea0c3d124fc57161fa2839cf50bd
Leo Stock <leo.stock@live.de>
2025-01-13 18:52:59 +0000

    disable footer for now

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,18 +1,18 @@
-import Link from "next/link";
+// import Link from "next/link";
 
 const Footer = () => {
     return (
         <div
             className='w-full lg:flex flex-row justify-between items-center p-6 lg:py-3'
             style={{ backgroundColor: "#000000" }}>
-            <div className='flex flex-row mb-2 lg:mb-0'>
+            {/* <div className='flex flex-row mb-2 lg:mb-0'>
                 <div>© 2025 Lower m Ltd. • All rights reserved</div>
             </div>
             <div className='flex flex-col lg:flex-row  items-center lg:gap-4 gap-2'>
                 <Link href='#'>Privacy Policy</Link>
 
                 <Link href='#'>Terms of Use</Link>
-            </div>
+            </div> */}
         </div>
     );
 };
