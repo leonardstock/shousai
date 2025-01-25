f348410bd4ebecbf299f31734cce2f5e226dcc2f
Leo Stock <leo.stock@live.de>
2025-01-25 09:43:11 +0000

    middleware test

--- middleware.ts
+++ middleware.ts
@@ -11,6 +11,12 @@ const isUnProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
+    const url = req.nextUrl.pathname;
+
+    if (url === "/api/webhooks/clerk") {
+        return; // Skip authentication for this route
+    }
+
     if (!isUnProtectedRoute(req)) await auth.protect();
 });
 
@@ -19,6 +25,6 @@ export const config = {
         // Skip Next.js internals and all static files, unless found in search params
         "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
         // Always run for API routes
-        "/(api|trpc)(?!/webhooks/clerk).*",
+        "/(api|trpc)(.*)",
     ],
 };
